06e0bda7b6dae79e4aef0d21c366726e1b892ddc
vince <vjmalicsi08@gmail.com>
2025-08-28 08:07:44 +0800

    sinimulan server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express'; 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+    console.log(`port ${PORT} is running`);
+});
+
+
+
+    
+    
+    
