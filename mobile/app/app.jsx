aadfae31c635e6e5cbb99e8683e383de60d9ebd7
Neowski <reynesanthony08@gmail.com>
2025-08-31 23:34:15 +0800

    inayos lang

---
+++ mobile/app/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 24 }}>Rental System App</Text>
+    </View>
+  );
+}
