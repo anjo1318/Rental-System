7d46118336b98d1c7b3e929630a58f15bd205978
Vince <vjmalicsi08@gmail.com>
2026-02-23 09:32:57 +0800

    push for possible fix sa server

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -12,7 +12,9 @@ import sequelize from "../database/database.js";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: process.env.SMTP_HOST,      // smtp.gmail.com
+  port: parseInt(process.env.SMTP_PORT), // 587
+  secure: false,                     // false for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
